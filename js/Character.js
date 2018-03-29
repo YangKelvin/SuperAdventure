359f31f242fb2172dd3a6399858aa5d530b1a736
YangKelvin <k31093109@gmail.com>
2018-03-29 11:56:51 +0800

    version 1.10

---
+++ js/Character.js
@@ -0,0 +1,119 @@
+class Character
+{
+    constructor()
+    {
+        this.component
+        this.arraySize = []
+        this.mbox2D
+        this.isDead = false
+        this.isWalking = 0
+        this.isJump = false
+    }
+    
+    
+    get position()
+    {
+        return this.component.position
+    }
+    set position(newValue)
+    {
+        this.component.position = newValue
+    }
+
+    get scale()
+    {
+        return this.component.scale
+    }
+    set scale(newValue)
+    {
+        this.component.scale = newValue
+    }
+
+    get rotation()
+    {
+        return this.component.rotation
+    }
+    set rotation(newValue)
+    {
+        this.component.rotation = newValue
+    }
+
+    get isSensor()
+    {
+        return this.component.isSensor
+    }
+    set isSensor(newValue)
+    {
+        this.component.isSensor = newValue
+    }
+
+    init(sprite, box2D) 
+    {
+        this.mbox2D = box2D;
+        this.pic = new Framework.Sprite(define.imagePath + sprite)
+        //this.component = new Framework.circleComponent(this.pic, box2D.bodyType_Dynamic, box2D)
+        this.component = new Framework.squareComponent(this.pic, box2D.bodyType_Dynamic, box2D)
+        this.component.fixtureDef.m_restitution = 0
+        this.component.Body.m_userData = "hero"
+    }
+
+    update(isWalking) 
+    {
+        //0 停
+        //1 右
+        //2 左
+        console.log(this.isWalking)
+        if (this.isWalking === 1)
+        {
+            this.goRight()
+        }
+        if (this.isWalking === 2)
+        {
+            this.goLeft()
+        }
+
+
+        this.component.update()
+    }
+    draw() 
+    {
+        this.pic.draw()
+    }
+
+    move(angle)
+    {
+        
+    }
+    goRight()
+    { 
+        
+        //this.hero.position(this.hero.position()+10)
+        
+        var power = 100
+        this.component.Body.ApplyForce(
+            new this.mbox2D.b2Vec2(
+                                Math.cos(0 * (Math.PI / 180)) * power, 
+                                Math.sin(0 * (Math.PI / 180)) * power),
+                                this.component.Body.GetWorldCenter())
+        
+    }
+    goLeft()
+    {
+        var power = 100
+        this.component.Body.ApplyForce(
+            new this.mbox2D.b2Vec2(
+                                Math.cos(180 * (Math.PI / 180)) * power, 
+                                Math.sin(180 * (Math.PI / 180)) * power),
+                                this.component.Body.GetWorldCenter())
+    }
+    
+    jump()
+    {
+        var power = 15000
+        this.component.Body.ApplyForce(
+            new this.mbox2D.b2Vec2(
+                                Math.cos(90 * (Math.PI / 180)) * power, 
+                                Math.sin(90 * (Math.PI / 180)) * power),
+                                this.component.Body.GetWorldCenter())
+    }
+}
---
+++ js/Level1.js
@@ -0,0 +1,129 @@
+var Level1 = Framework.Class(Framework.Level, 
+    {
+        load : function () 
+        {
+            //#region box2D
+            this.box2D = new Framework.Box2D()
+            this.world = this.box2D.createWorld()
+            this.box2D.setContactListener()
+            //#endregion
+
+            //#region 
+            var ground = this.box2D.createSquareBody(35, 1.0, this.box2D.bodyType_Static)
+            ground.SetPosition(new this.box2D.b2Vec2(10,26))
+            //#endregion
+
+            //#region map
+            this.mapfloorValue = 
+            [
+                {x: 30, y: 782},
+                {x: 95, y: 782},
+                {x: 155, y: 782},
+                {x: 215, y: 782},
+                {x: 275, y: 782},
+                {x: 335, y: 782},
+                {x: 395, y: 782},
+                {x: 455, y: 782},
+                {x: 515, y: 782},
+                {x: 575, y: 782},
+                {x: 635, y: 782},
+                {x: 695, y: 782},
+                {x: 755, y: 782},
+                {x: 815, y: 782},
+                {x: 875, y: 782},
+                {x: 935, y: 782},
+                {x: 995, y: 782},
+                {x: 1055, y: 782},
+                {x: 1115, y: 782},
+                {x: 1175, y: 782},
+                {x: 1235, y: 782},
+                {x: 1295, y: 782},
+            ]
+
+            this.mapfloor = new Array()
+            for (var i = 0; i < this.mapfloorValue.length; i++)
+            {
+                this.mapfloor[i] = new floor()
+                this.mapfloor[i].init('floor2.png', this.box2D)
+                this.mapfloor[i].position = 
+                {
+                    x: this.mapfloorValue[i].x,
+                    y: this.mapfloorValue[i].y
+                }
+                this.mapfloor[i].scale = 2
+                this.mapfloor[i].rotation = 0
+            }
+            //#endregion
+
+            //#region Character
+            this.hero = new Character()
+            this.hero.init('Character2.png', this.box2D)
+            this.hero.position =
+            {
+                x: 180,
+                y: 300
+            }
+            this.hero.pic.scale = 0.2
+            //#endregion
+        },
+        
+        initialize : function () 
+        {
+
+
+            for	(var i = 0; i<this.mapfloor.length; i++)
+            {
+                this.rootScene.attach(this.mapfloor[i].pic)
+            }
+
+            this.rootScene.attach(this.hero.pic)
+        },
+
+        update : function () 
+        {
+            this.box2D.draw()
+            this.hero.update()
+        },
+        draw : function (parentCtx) 
+        {
+           // this.map.draw();
+            this.box2D.draw()
+            this.rootScene.draw()
+        },
+
+        keydown(e, list)
+        {
+            var heroPosition = this.hero.position;
+            if (e.key === 'Right')
+            {
+                console.log("Right")
+                this.hero.isWalking = 1
+                //console.log(this.hero.component.sprite.rotation)
+                //this.hero.goRight()
+                //this.hero.update(1)
+            }
+            if (e.key ==='Left')
+            {
+                console.log("Left")
+                this.hero.isWalking = 2
+                //console.log(this.hero.rotation)
+                //this.hero.goLeft()
+                
+                //this.hero.update(2)
+            }
+            if (e.key === 'Space')
+            {
+                console.log("Jump")
+                this.hero.jump()
+            }
+        },
+        keyup(e, list)
+        {
+            if(e.key === 'Left' || e.key === 'Right')
+            {
+                console.log("AAA")
+                this.hero.isWalking = 0;
+            }
+        }
+    });
+    
